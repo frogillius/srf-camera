6628708c54d4e0800178ce450f4a3a7ab99d8f12
frogillius <151787619+frogillius@users.noreply.github.com>
2024-10-22 16:43:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install python3-pip libgtk2.0-dev pkg-install
 pip3 install torch torchvision torchaudio
-pip3 install opencv-python pandas requests
+pip3 install opencv-python pandas requests serial pyserial
 pip3 install matplotlib  # Optional, for visualization
 
 # prep
